2b26ca69f0f87fc83740c62098d614bd6a16342a
aiecel <igorperepechin101@gmail.com>
2022-10-02 16:03:59 +0400

    Different restroom icons

--- crap-advisor-frontend/src/core/components/RestroomMarker/index.tsx
+++ crap-advisor-frontend/src/core/components/RestroomMarker/index.tsx
@@ -1,22 +1,35 @@
 import React from "react";
 import {Marker, Tooltip, useMap} from "react-leaflet";
 import L from "leaflet";
-import restroomImage from "../../../assets/restroomMarker.png";
 import "./index.css";
 import {Restroom} from "../../../app/typings";
 import {IMapProps} from "../Map";
 import {LeafletEvent, LatLngExpression} from 'leaflet'
+
+import restroom1 from "../../../assets/restroom_2.png";
+import restroom2 from "../../../assets/restroom_2.png";
+import restroom3 from "../../../assets/restroom_4.png";
+import restroom4 from "../../../assets/restroom_4.png";
+import restroom5 from "../../../assets/restroom_5.png";
 
 interface IRestroomMakerProps extends Pick<IMapProps, 'selectRestroom'> {
     restroom: Restroom
 }
 
 const RestroomMarker = ({restroom, selectRestroom}: IRestroomMakerProps) => {
+
+    const iconImages = [restroom1, restroom2, restroom3, restroom4, restroom5]
+    let currentImage = restroom4
+
+    if (restroom.rating) {
+        currentImage = iconImages[Math.round(restroom.rating) - 1]
+    }
+
     const icon = L.divIcon({
         className: "icon",
         html:
             `<div class="marker-container">` +
-            `<img class="marker-icon" src='${restroomImage}' alt='Toilet'/>` +
+            `<img class="marker-icon" src='${currentImage}' alt='Toilet'/>` +
             `<p class="marker-text outlined">${restroom.rating ?? ""}</p>` +
             `</div>`,
     });
