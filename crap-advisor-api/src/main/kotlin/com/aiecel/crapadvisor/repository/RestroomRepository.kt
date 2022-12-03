4a155c85a7308c74c7bd298ca2f81a54cafe9625
aiecel <igorperepechin101@gmail.com>
2022-12-03 22:14:15 +0400

    Smol fix

--- crap-advisor-api/src/main/kotlin/com/aiecel/crapadvisor/repository/RestroomRepository.kt
+++ crap-advisor-api/src/main/kotlin/com/aiecel/crapadvisor/repository/RestroomRepository.kt
@@ -3,4 +3,4 @@ package com.aiecel.crapadvisor.repository
 import com.aiecel.crapadvisor.model.entity.Restroom
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface RestroomRepository: JpaRepository<Restroom, Long>
+interface RestroomRepository : JpaRepository<Restroom, Long>
